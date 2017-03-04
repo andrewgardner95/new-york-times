8a4df2ef9523516b4d6ca2584dea0fd1d505abf0
Andrew Gardner <andrewgardner@Andrews-MacBook-Air-2.local>
2017-03-04 15:44:44 -0800

    php and json files added

--- nyTimes.js
+++ nyTimes.js
@@ -1,4 +1,3 @@
-// http://nytarticle-search.herokuapp.com/
 
 var searchTerm = $("#search-term").val().trim();
 var recordCount = parseInt($("#record-count").val().trim());
@@ -6,10 +5,10 @@ var startYear = parseInt($("#start-year").val().trim());
 var endYear = parseInt($("#end-year").val().trim());
 
 
- //Event listener for all buttons
-    $(".fields").on("click", function() {
-      var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
-      var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+//Event listener for all buttons
+  $(".fields").on("click", function() {
+    var authKey = "b9f91d369ff59547cd47b931d8cbc56b:0:74623931";
+    var queryURLBase = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
   authKey + "&q=" + searchTerm + "&;
 
   console.log(queryURLBase);
